42976fdded722f1a01f40c7558601b1045753005
Danh Tran <12520054@users.noreply.github.com>
2019-01-22 14:52:19 +0700

    Update com.creative.brickbreaker.ads
    change admob

--- com.creative.brickbreaker.ads
+++ com.creative.brickbreaker.ads
@@ -2,7 +2,7 @@
   "admob_banner_ios_id": "admob_banner_ios_id",
   "admob_fullscreen_ios_id": "admob_fullscreen_ios_id",
   "admob_video_reward_ios_id": "admob_video_reward_ios_id",
-  "admob_banner_android_id": "/21617116612/134471542713620",
-  "admob_fullscreen_android_id": "/21617116612/334471542713620",
-  "admob_video_reward_android_id": "/112517806/634471543128979"
+  "admob_banner_android_id": "ca-app-pub-2313206220567592/1797983517",
+  "admob_fullscreen_android_id": "ca-app-pub-2313206220567592/9293330151",
+  "admob_video_reward_android_id": "ca-app-pub-2313206220567592/5969064753"
 }
