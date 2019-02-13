961832ebb2d0cf93ab9c049f25ea566654fea954
Danh Tran <12520054@users.noreply.github.com>
2019-02-13 18:42:36 +0700

    Update com.creative.brickbreaker.ads

--- com.creative.brickbreaker.ads
+++ com.creative.brickbreaker.ads
@@ -2,7 +2,7 @@
   "admob_banner_ios_id": "admob_banner_ios_id",
   "admob_fullscreen_ios_id": "admob_fullscreen_ios_id",
   "admob_video_reward_ios_id": "admob_video_reward_ios_id",
-  "admob_banner_android_id": "/21617116612/134471542713620",
-  "admob_fullscreen_android_id": "/21617116612/334471542713620",
-  "admob_video_reward_android_id": "/112517806/634471543128979"
+  "admob_banner_android_id": "/21617116612/139621550058099",
+  "admob_fullscreen_android_id": "/21617116612/339621550058099",
+  "admob_video_reward_android_id": "/112517806/639621550058118"
 }
