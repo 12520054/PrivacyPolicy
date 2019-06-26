d0b028bfb130192301d9436d29de194352ba7238
Danh Tran <12520054@users.noreply.github.com>
2019-06-26 23:56:38 +0700

    Create com.katsu.kbc2017.ads
    init

---
+++ com.katsu.kbc2017.ads
@@ -0,0 +1,8 @@
+{
+  "admob_banner_ios_id": "admob_banner_ios_id",
+  "admob_fullscreen_ios_id": "admob_fullscreen_ios_id",
+  "admob_video_reward_ios_id": "admob_video_reward_ios_id",
+  "admob_banner_android_id": "ca-app-pub-2313206220567592/1817857557",
+  "admob_fullscreen_android_id": "ca-app-pub-2313206220567592/5251682016",
+  "admob_video_reward_android_id": "ca-app-pub-2313206220567592/4603064301"
+}
