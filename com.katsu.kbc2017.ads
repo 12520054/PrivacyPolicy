a40a1aab8053df9fd4d0e97c53414d48da7a9945
Danh Tran <12520054@users.noreply.github.com>
2019-07-03 01:59:23 +0700

    Update com.katsu.kbc2017.ads

--- com.katsu.kbc2017.ads
+++ com.katsu.kbc2017.ads
@@ -3,6 +3,6 @@
   "admob_fullscreen_ios_id": "admob_fullscreen_ios_id",
   "admob_video_reward_ios_id": "admob_video_reward_ios_id",
   "admob_banner_android_id": "ca-app-pub-2313206220567592/1817857557",
-  "admob_fullscreen_android_id": "ca-app-pub-3940256099942544/1033173712",
+  "admob_fullscreen_android_id": "ca-app-pub-2313206220567592/5251682016",
   "admob_video_reward_android_id": "ca-app-pub-2313206220567592/4603064301"
 }
