276f0ce81781acdfb3c5a96a344e53b500fe86f6
Danh Tran <12520054@users.noreply.github.com>
2019-07-04 21:56:55 +0700

    Update com.katsu.kbc2017.ads
    change to adx

--- com.katsu.kbc2017.ads
+++ com.katsu.kbc2017.ads
@@ -2,7 +2,7 @@
   "admob_banner_ios_id": "admob_banner_ios_id",
   "admob_fullscreen_ios_id": "admob_fullscreen_ios_id",
   "admob_video_reward_ios_id": "admob_video_reward_ios_id",
-  "admob_banner_android_id": "ca-app-pub-2313206220567592/1817857557",
-  "admob_fullscreen_android_id": "ca-app-pub-2313206220567592/5251682016",
-  "admob_video_reward_android_id": "ca-app-pub-2313206220567592/4603064301"
+  "admob_banner_android_id": "/21617015150/52210046/21830796256",
+  "admob_fullscreen_android_id": "/21617015150/52210046/21830726238",
+  "admob_video_reward_android_id": "/21617015150/52210046/21830796346"
 }
